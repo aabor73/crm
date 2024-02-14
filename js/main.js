9c00f8dcbd1a227675b93adf319238f10e68df83
aabor73 <aabor73@yandex.ru>
2024-02-14 19:38:49 +0300

    lesson_02

---
+++ js/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const modalTitle = document.querySelector('.modal-title');
+const modalForm = document.querySelector('.modal-form');
+const checkboxDiscount = document.querySelector('.modal-checkbox');
+const inputDiscount = document.querySelector('.modal__input_discount');
